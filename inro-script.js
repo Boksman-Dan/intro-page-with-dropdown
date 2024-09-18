31869a84d1e5dbca8668f78fdad97ae7912f7719
Boksman-Dan <omejedanielkelechi@gmail.com>
2024-09-18 22:44:06 +0100

    Add files via upload

---
+++ inro-script.js
@@ -0,0 +1,49 @@
+
+// GET THE HTML ELEMENTS
+const btnOpenEl = document.querySelector('.btn-open');
+const btnCloseEl = document.querySelector('.btn-close');
+const asideContentEl = document.querySelector('.aside-content');
+const mainContentEl =  document.querySelector('.main-content');
+const overlayEl = document.querySelector('.overlay');
+const timeNowEl = document.querySelector('.time-now');
+const dateNowEl = document.querySelector('.date-now');
+
+
+// FUNCTION TO OPEN AND CLOSE SIDE CONTENT
+
+const openSideContent = function () {
+  asideContentEl.classList.add('right-nav');
+  overlayEl.classList.remove('hidden');
+};
+
+const closeSideContent = function () {
+  asideContentEl.classList.remove('right-nav');
+  //mainContentEl.style.marginLeft = '0rem';
+  overlayEl.classList.add('hidden');
+};
+
+btnOpenEl.addEventListener('click', openSideContent);
+overlayEl.addEventListener('click', closeSideContent);
+btnCloseEl.addEventListener('click', closeSideContent);
+
+/*
+// GET THE CURRENT TIME AND DAY
+
+const date = new Date();
+const day = date.getDay();
+const currentHours = date.getHours();
+const currentMins = date.getMinutes();
+
+const getCurrentTime = currentHours >= 12 ? `${currentHours} : ${currentMins} PM` : `${currentHours} : ${currentMins} AM`;
+
+timeNowEl.textContent = getCurrentTime;
+
+const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+for (let i = 0; i <= days.length; i++) {
+  if (day === i) {
+    // console.log(days[i]);
+    dateNowEl.textContent = `Today is: ${days[i]}`;
+  }
+}
+  */
